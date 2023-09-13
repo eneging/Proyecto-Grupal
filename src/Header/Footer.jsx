cb1476f196446b8015d5bf9f760e90db7cceb7e9
ANDRES FELIPE MEDINA MUÑOZ <107763296+felipemedina05@users.noreply.github.com>
2023-09-13 12:09:14 -0500

    creando esquelleto footer

--- src/Header/Footer.jsx
+++ src/Header/Footer.jsx
@@ -2,7 +2,31 @@
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div>
+      <footer>
+          <div>
+              <h2>sunnyside</h2>
+              <ul>
+                  <li>About</li>
+                  <li>Services</li>
+                  <li>Projects</li>
+              </ul>
+              <ul>
+                  <li><a href="#"><img src="" alt="facebook"/></a></li>
+                  <li><a href="#"><img src="" alt="instagram"/></a></li>
+                  <li><a href="#"><img src="" alt="twiter"/></a></li>
+                  <li><a href="#"><img src="" alt="Pintrest"/></a></li>
+              </ul>
+              
+          </div>
+      </footer>
+
+
+
+
+    </div>
+    </>
   )
 }
 
