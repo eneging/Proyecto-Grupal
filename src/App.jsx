67cbd3aa1e4212c4580a0bed53aa3d4c22eaa73a
JeanPierre <jampis9@hotmail.com>
2023-09-13 12:55:08 -0500

    listo Jean-Pierre

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Main2 from './Header/Main2'
-
+import imgfresa from "./assets/desktop/image-graphic-design.jpg";
+import imgnaranja from "./assets/desktop/image-photography.jpg";
 function App() {
  
 
   return (
     <>
-      <Main2></Main2>
+      <Main2 fnImg1= {imgfresa} fnImg2= {imgnaranja} ></Main2>
    
     </>
   )
--- src/Header/Main2.jsx
+++ src/Header/Main2.jsx
@@ -1,14 +1,25 @@
-
-function Main2() {
+import "./Main2.css";
+function Main2(props) {
   return (
     <div id="principal">
-<div>
-  <img src="" alt="" srcset="" />
-  
-</div>
-
+      <div id="img1">
+        <img id="imagen" src={props.fnImg1} alt="..." />
+        <h2 id="titulo">Graphic Design</h2>
+        <p id="description">
+          Great design makes you memorable. We deliver artwork that underscores
+          your brand message and captures potential clients attention
+        </p>
+      </div>
+      <div id="img2">
+        <img id="imagen" src={props.fnImg2} alt="..." />
+        <h2 id="titulo">Photography</h2>
+        <p id="description">
+          Increase your credibility by getting the most stunning, high-quality
+          photos that improve your business image.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Main2
+export default Main2;
