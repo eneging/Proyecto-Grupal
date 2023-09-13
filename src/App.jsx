367979a47b7853dfae3d5b049763eae60a08cef4
AlejandroBonilla2000 <141190335+AlejandroBonilla2000@users.noreply.github.com>
2023-09-13 13:01:50 -0500

    trabajar el main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import './Header/Main.jsx'
+import Main from './Header/Main.jsx'
 
 function App() {
  
@@ -7,6 +9,7 @@ function App() {
   return (
     <>
       <h1>hola , mundo</h1>
+      <Main></Main>
    
     </>
   )
