89f1704e015d9fbc9c7a1e261bab39d3bb8b56bb
teobaldo <teobaldo.navarro1@gmail.com>
2023-09-13 12:24:51 -0500

    colocando imagen

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,7 +2,7 @@
 
 function Header() {
   return (
-    <div>
+    <div><img src="image-haader.jpg" />
         <h1>Header</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At enim dolor sequi quo vitae ad blanditiis assumenda ipsum nulla velit delectus, iste suscipit aspernatur tempore ullam! Vitae ratione sint alias nulla at quibusdam perspiciatis fugit deleniti magnam hic iste consequatur incidunt asperiores accusantium quasi illo reiciendis magni, vel necessitatibus iusto voluptatum. Eaque necessitatibus enim quibusdam similique non molestias, tempora corrupti tempore ipsa aperiam accusamus? Animi praesentium unde officiis, amet, quasi inventore sed ducimus, cumque voluptatibus odio ipsum error laudantium! Quos sequi minima tempore totam, numquam dolores perspiciatis repellat, distinctio eaque similique necessitatibus non consequuntur ab reiciendis fuga veritatis, suscipit amet.</p>
     </div>
